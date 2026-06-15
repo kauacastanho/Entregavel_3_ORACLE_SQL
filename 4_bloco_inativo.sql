bf2f86f5e04958604db9de7b8cdc4f66f3293c9e
Kauã Castanho Theobald <164340608+kauacastanho@users.noreply.github.com>
2026-06-15 13:47:15 -0300

    Add files via upload

--- 4_bloco_inativo.sql
+++ 4_bloco_inativo.sql
@@ -3,10 +3,12 @@ DECLARE
 BEGIN
     FOR item IN (SELECT id FROM clientes_e3) LOOP
         v_apoio := clientes_pkg.inativo(item.id);
-        
-        IF v_apoio = 'V' THEN
-            DBMS_OUTPUT.PUT_LINE('Cliente ' || item.id || ' inativado.');
-        END IF;
-        
     END LOOP;
 END;
+
+
+SHOW USER;
+
+SELECT username
+FROM all_users
+ORDER BY username;
