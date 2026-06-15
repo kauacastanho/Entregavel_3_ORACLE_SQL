81b0d3167731ff5827fb44e859f46438fe903202
Kauã Castanho Theobald <164340608+kauacastanho@users.noreply.github.com>
2026-06-15 12:51:10 -0300

    Update 4_bloco_inativo.sql

--- 4_bloco_inativo.sql
+++ 4_bloco_inativo.sql
@@ -3,7 +3,10 @@ DECLARE
 BEGIN
     FOR item IN (SELECT id FROM clientes_e3) LOOP
         v_apoio := clientes_pkg.inativo(item.id);
+        
+        IF v_apoio = 'V' THEN
+            DBMS_OUTPUT.PUT_LINE('Cliente ' || item.id || ' inativado.');
+        END IF;
+        
     END LOOP;
 END;
-
-
