7cf580c3cf58adf32883fb54b71468ed4e88fe11
Kauã Castanho Theobald <164340608+kauacastanho@users.noreply.github.com>
2026-06-15 10:52:37 -0300

    Add files via upload

---
+++ 3_bloco_vendas.sql
@@ -0,0 +1,18 @@
+BEGIN
+    faturamento_pkg.processar_pedido(1);
+    faturamento_pkg.processar_pedido(9);
+    faturamento_pkg.processar_pedido(27);
+    faturamento_pkg.processar_pedido(47);
+    faturamento_pkg.processar_pedido(14);
+    faturamento_pkg.processar_pedido(9);
+    faturamento_pkg.processar_pedido(27);
+    faturamento_pkg.processar_pedido(47);
+    faturamento_pkg.processar_pedido(14);
+    faturamento_pkg.processar_pedido(9);
+    faturamento_pkg.processar_pedido(27);
+    faturamento_pkg.processar_pedido(9);
+    faturamento_pkg.processar_pedido(19);
+    faturamento_pkg.processar_pedido(41);
+    faturamento_pkg.processar_pedido(8);
+END;   
+ 
