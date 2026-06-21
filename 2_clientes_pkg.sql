dc33fe63fd7fd1c519fa223992842ab355f0cf74
Joao Pedro <1139038@atitus.edu.br>
2026-06-20 23:13:41 -0300

    Refactor clientes_pkg to remove parameter from inativar_clientes

--- 2_clientes_pkg.sql
+++ 2_clientes_pkg.sql
@@ -1,54 +1,60 @@
 CREATE OR REPLACE PACKAGE clientes_pkg AS
     FUNCTION inativo(p_id_cliente IN NUMBER) RETURN CHAR;
-    PROCEDURE inativar_clientes(p_id_cliente IN NUMBER);
+    PROCEDURE inativar_clientes;
 END;
 /
 
 CREATE OR REPLACE PACKAGE BODY clientes_pkg AS
 
-    -- Procedure que inativa clientes se a função inativo() for verdadeira
-    PROCEDURE inativar_clientes (
-        p_id_cliente NUMBER
-    )IS
-        v_ativo NUMBER;
-    BEGIN
-        SELECT ativo 
-        INTO v_ativo
-        FROM clientes_e3
-        WHERE id = p_id_cliente;
-        
-        IF v_ativo = 1 THEN
-            UPDATE clientes_e3 SET ativo = 0 WHERE id = p_id_cliente;
-        END IF;
-        
-        EXCEPTION
-            WHEN NO_DATA_FOUND THEN
-                DBMS_OUTPUT.PUT_LINE('Cliente ' || p_id_cliente || ' não encontrado');
-
-            WHEN OTHERS THEN
-                DBMS_OUTPUT.PUT_LINE('Erro ao inativar cliente: ' || SQLERRM);
-    END;
-
-    -- Função que retorna V caso o cliente não tenha pedidos nos últimos 365 dias
-    FUNCTION inativo (
-        p_id_cliente NUMBER
-    )RETURN CHAR
+    FUNCTION inativo(
+        p_id_cliente IN NUMBER
+    ) RETURN CHAR
     IS
         v_qtd_pedidos NUMBER;
     BEGIN
         SELECT COUNT(*)
         INTO v_qtd_pedidos
         FROM pedidos_e3
-        WHERE id_cliente = p_id_cliente AND data_pedido >= SYSDATE - 365;
-        
+        WHERE id_cliente = p_id_cliente
+        AND data_pedido >= SYSDATE - 365;
+
         IF v_qtd_pedidos = 0 THEN
-            inativar_clientes(p_id_cliente);
             RETURN 'V';
         ELSE
             RETURN 'F';
         END IF;
+
+    EXCEPTION
+        WHEN OTHERS THEN
+            RETURN 'F';
     END;
-    
+
+
+    PROCEDURE inativar_clientes
+    IS
+    BEGIN
+        FOR cliente IN (
+            SELECT id
+            FROM clientes_e3
+        ) LOOP
+
+            IF inativo(cliente.id) = 'V' THEN
+                UPDATE clientes_e3
+                SET ativo = 0
+                WHERE id = cliente.id;
+
+                DBMS_OUTPUT.PUT_LINE('Cliente ' || cliente.id || ' inativado.');
+            END IF;
+
+        END LOOP;
+
+        COMMIT;
+
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+            DBMS_OUTPUT.PUT_LINE('Erro ao inativar clientes: ' || SQLERRM);
+    END;
+
 END clientes_pkg;
 /
-
