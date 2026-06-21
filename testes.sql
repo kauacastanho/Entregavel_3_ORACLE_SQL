2533d6861e76799380ea0c3d90fd998262dea313
Kauã Castanho Theobald <164340608+kauacastanho@users.noreply.github.com>
2026-06-21 18:56:39 -0300

    Update testes.sql

--- testes.sql
+++ testes.sql
@@ -1,12 +1,12 @@
 BEGIN
-    JOAO_20261.faturamento_pkg.processar_pedido(1);
-    JOAO_20261.faturamento_pkg.processar_pedido(9);
-    JOAO_20261.faturamento_pkg.processar_pedido(12);
-    JOAO_20261.faturamento_pkg.processar_pedido(16);
-    JOAO_20261.faturamento_pkg.processar_pedido(13);
-    JOAO_20261.faturamento_pkg.processar_pedido(5);
-    JOAO_20261.faturamento_pkg.processar_pedido(48);
-    JOAO_20261.faturamento_pkg.processar_pedido(50);
+    JOAOPEDRO_20261.faturamento_pkg.processar_pedido(1);
+    JOAOPEDRO_20261.faturamento_pkg.processar_pedido(9);
+    JOAOPEDRO_20261.faturamento_pkg.processar_pedido(12);
+    JOAOPEDRO_20261.faturamento_pkg.processar_pedido(16);
+    JOAOPEDRO_20261.faturamento_pkg.processar_pedido(13);
+    JOAOPEDRO_20261.faturamento_pkg.processar_pedido(5);
+    JOAOPEDRO_20261.faturamento_pkg.processar_pedido(48);
+    JOAOPEDRO_20261.faturamento_pkg.processar_pedido(50);
 END;   
 / 
  
@@ -14,7 +14,7 @@ DECLARE
     v_resultado CHAR;
 BEGIN
     FOR item IN (SELECT id FROM clientes_e3) LOOP
-        v_resultado := JOAO_20261.clientes_pkg.inativo(item.id);
+        v_resultado := JOAOPEDRO_20261.clientes_pkg.inativo(item.id);
         
         DBMS_OUTPUT.PUT_LINE('Cliente ' || item.id ||' -> ' || v_resultado);
     END LOOP;
@@ -25,7 +25,7 @@ DECLARE
     v_apoio CHAR;
 BEGIN
     FOR item IN (SELECT id FROM clientes_e3) LOOP
-        v_apoio := JOAO_20261.clientes_pkg.inativo(item.id);
+        v_apoio := JOAOPEDRO_20261.clientes_pkg.inativo(item.id);
         
         IF v_apoio = 'V' THEN
             DBMS_OUTPUT.PUT_LINE('Cliente ' || item.id || ' inativado.');
