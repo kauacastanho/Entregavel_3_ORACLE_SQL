0db9b70f457849d0d320c1eef298cca1f793b00d
Joao Pedro <1139038@atitus.edu.br>
2026-06-20 23:19:59 -0300

    Update 3_bloco_vendas.sql

--- 3_bloco_vendas.sql
+++ 3_bloco_vendas.sql
@@ -1,18 +1,22 @@
+DECLARE
+    v_id_pedido NUMBER;
+    v_cliente NUMBER;
 BEGIN
-    faturamento_pkg.processar_pedido(1);
-    faturamento_pkg.processar_pedido(9);
-    faturamento_pkg.processar_pedido(27);
-    faturamento_pkg.processar_pedido(47);
-    faturamento_pkg.processar_pedido(14);
-    faturamento_pkg.processar_pedido(9);
-    faturamento_pkg.processar_pedido(27);
-    faturamento_pkg.processar_pedido(47);
-    faturamento_pkg.processar_pedido(14);
-    faturamento_pkg.processar_pedido(9);
-    faturamento_pkg.processar_pedido(27);
-    faturamento_pkg.processar_pedido(9);
-    faturamento_pkg.processar_pedido(19);
-    faturamento_pkg.processar_pedido(41);
-    faturamento_pkg.processar_pedido(8);
-END;   
- 
+    FOR i IN 1..15 LOOP
+
+        v_cliente := MOD(i, 15) + 1;
+
+        INSERT INTO pedidos_e3(id_cliente, data_pedido)
+        VALUES (v_cliente, SYSDATE)
+        RETURNING id INTO v_id_pedido;
+
+        INSERT INTO pedido_itens_e3(id_pedido, id_produto, quantidade, preco_unit)
+        VALUES (v_id_pedido, 1, 1, 3500);
+
+        faturamento_pkg.processar_pedido(v_id_pedido);
+
+        DBMS_OUTPUT.PUT_LINE('-------------------------');
+
+    END LOOP;
+END;
+/
